3316f711f3acf04d00acdcb1a8d075faefc6322b
habin <danny9643@naver.com>
2024-08-15 19:47:36 +0900

    Test Base Config 추가

---
+++ src/test/java/com/artinus/channelsubscription/channel/controller/ChannelControllerTest.java
@@ -0,0 +1,9 @@
+package com.artinus.channelsubscription.channel.controller;
+
+import com.artinus.channelsubscription.base.ControllerBaseTest;
+
+class ChannelControllerTest extends ControllerBaseTest {
+
+
+
+}
