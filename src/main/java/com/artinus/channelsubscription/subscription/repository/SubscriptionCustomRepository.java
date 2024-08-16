878274e427fe69df83f5330e78b5ea35852a07d2
habin <danny9643@naver.com>
2024-08-16 22:22:41 +0900

    채널 이력 조회 API 구현

--- src/main/java/com/artinus/channelsubscription/subscription/repository/SubscriptionCustomRepository.java
+++ src/main/java/com/artinus/channelsubscription/subscription/repository/SubscriptionCustomRepository.java
@@ -3,6 +3,7 @@ package com.artinus.channelsubscription.subscription.repository;
 import com.artinus.channelsubscription.subscription.domain.SubscriptionHistory;
 import jakarta.validation.constraints.NotBlank;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Map;
 
@@ -10,4 +11,5 @@ public interface SubscriptionCustomRepository {
 
     Map<String, List<SubscriptionHistory>> findAllByPhoneNumber(@NotBlank String phoneNumber);
 
+    List<SubscriptionHistory> findAllByChannelIdAndDate(Long channelId, LocalDate date);
 }
