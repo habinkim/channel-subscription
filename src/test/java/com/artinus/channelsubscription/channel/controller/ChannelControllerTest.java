d05757f71385f8e7ef186fafbc9d9e59e3707e68
habin <danny9643@naver.com>
2024-08-17 16:20:09 +0900

    채널 이력 조회 API 테스트 작성

--- src/test/java/com/artinus/channelsubscription/channel/controller/ChannelControllerTest.java
+++ src/test/java/com/artinus/channelsubscription/channel/controller/ChannelControllerTest.java
@@ -1,26 +1,45 @@
 package com.artinus.channelsubscription.channel.controller;
 
 import com.artinus.channelsubscription.base.ControllerBaseTest;
-import com.artinus.channelsubscription.channel.domain.RegisterChannelRequest;
 import com.artinus.channelsubscription.channel.domain.ChannelType;
+import com.artinus.channelsubscription.channel.domain.RegisterChannelRequest;
+import com.artinus.channelsubscription.channel.domain.RegisteredChannel;
+import com.artinus.channelsubscription.channel.service.ChannelService;
 import com.artinus.channelsubscription.common.response.MessageCode;
+import com.artinus.channelsubscription.subscription.domain.SubscribeRequest;
+import com.artinus.channelsubscription.subscription.domain.SubscriptionStatus;
+import com.artinus.channelsubscription.subscription.service.SubscriptionService;
 import org.apache.commons.lang3.ArrayUtils;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.restdocs.payload.FieldDescriptor;
+import org.springframework.restdocs.request.ParameterDescriptor;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.stream.IntStream;
 
 import static com.epages.restdocs.apispec.ResourceDocumentation.resource;
 import static com.epages.restdocs.apispec.ResourceSnippetParameters.builder;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.notNullValue;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.request.RequestDocumentation.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 class ChannelControllerTest extends ControllerBaseTest {
+
+    @Autowired
+    private ChannelService channelService;
+
+    @Autowired
+    private SubscriptionService subscriptionService;
 
     @Transactional
     @Test
@@ -29,9 +48,9 @@ class ChannelControllerTest extends ControllerBaseTest {
     void registerChannelSuccess() throws Exception {
         RegisterChannelRequest request = new RegisterChannelRequest("KT 통신사", ChannelType.SUBSCRIBE_UNSUBSCRIBE);
 
-        FieldDescriptor[] requestDescriptors = new FieldDescriptor[] {
-            fieldWithPath("name").description("채널명"),
-            fieldWithPath("type").description("채널 유형 [SUBSCRIBE_UNSUBSCRIBE, SUBSCRIBE_ONLY, UNSUBSCRIBE_ONLY]")
+        FieldDescriptor[] requestDescriptors = new FieldDescriptor[]{
+                fieldWithPath("name").description("채널명"),
+                fieldWithPath("type").description("채널 유형 [SUBSCRIBE_UNSUBSCRIBE, SUBSCRIBE_ONLY, UNSUBSCRIBE_ONLY]")
         };
 
         FieldDescriptor[] responseDescriptors = ArrayUtils.addAll(
@@ -75,6 +94,61 @@ class ChannelControllerTest extends ControllerBaseTest {
                                 )
                         )
                 );
+    }
+
+    @Transactional
+    @Test
+    @Order(2)
+    @DisplayName("채널 이력 조회, 성공")
+    void getChannelSubscriptionHistorySuccess() throws Exception {
+        RegisterChannelRequest registerChannelRequest = new RegisterChannelRequest("홈쇼핑 고객센터", ChannelType.SUBSCRIBE_UNSUBSCRIBE);
+        RegisteredChannel registeredChannel = channelService.registerChannel(registerChannelRequest);
+
+        IntStream.rangeClosed(1, 20).forEach(i -> {
+            String phoneNumber = "010-1234-" + String.format("%04d", i);
+            SubscribeRequest subscribeRequest = new SubscribeRequest(phoneNumber, registeredChannel.id(), SubscriptionStatus.REGULAR);
+            subscriptionService.subscribe(subscribeRequest);
+        });
+
+        ParameterDescriptor pathParameterDescriptor = parameterWithName("channelId").description("채널 ID");
+
+        ParameterDescriptor queryParameterDescriptor = parameterWithName("date").description("조회일자 [yyyyMMdd]");
+
+
+        FieldDescriptor[] responseDescriptors = ArrayUtils.addAll(baseResponseFields,
+                fieldWithPath("data.[]").description("구독 이력 목록"),
+                fieldWithPath("data.[].subscription_id").description("구독 이력 ID"),
+                fieldWithPath("data.[].phone_number").description("휴대폰 번호"),
+                fieldWithPath("data.[].channel_id").description("채널 ID"),
+                fieldWithPath("data.[].channel_name").description("채널명"),
+                fieldWithPath("data.[].previous_status").description("이전 구독 상태 [NONE, REGULAR, PREMIUM]"),
+                fieldWithPath("data.[].status").description("현재 구독 상태 [NONE, REGULAR, PREMIUM]"),
+                fieldWithPath("data.[].created_at").description("생성일시")
+        );
+
+        mockMvc.perform(
+                        get("/channels/{channelId}/subscriptions", registeredChannel.id())
+                                .param("date", DateTimeFormatter.ofPattern("yyyyMMdd").format(LocalDate.now()))
+                )
+                .andExpectAll(baseAssertion(MessageCode.SUCCESS))
+                .andExpect(jsonPath("$.data", notNullValue()))
+                .andExpect(jsonPath("$.data.length()", is(20)))
+                .andDo(
+                        restDocs.document(
+                                pathParameters(pathParameterDescriptor),
+                                queryParameters(queryParameterDescriptor),
+                                responseFields(responseDescriptors),
+                                resource(
+                                        builder().
+                                                description("채널 구독 이력 조회").
+                                                pathParameters(pathParameterDescriptor).
+                                                queryParameters(queryParameterDescriptor).
+                                                responseFields(responseDescriptors)
+                                                .build()
+                                )
+                        )
+                );
+
     }
 
 }
